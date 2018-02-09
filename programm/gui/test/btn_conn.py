f3747fd28133e16134cb11e61613f420c695959c
Cassa <zaswed76@gmail.com>
2018-02-10 01:00:45 +0200

    fix mod graph

--- programm/gui/test/btn_conn.py
+++ programm/gui/test/btn_conn.py
@@ -1,23 +1,6 @@
+mid = [3, 4]
 
-import sys
-from PyQt5 import QtWidgets
+b1 = [1, 2, *mid, 5, 6]
 
-class Widget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.resize(500, 500)
-        self.btn = QtWidgets.QPushButton(self)
-        self.btn.setCheckable(True)
-        self.btn.setObjectName('tag_name')
-        self.btn.setAutoExclusive(True)
-        self.btn.clicked.connect(self.click_btn)
 
-    def click_btn(self):
-        print("btn")
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    # app.setStyleSheet(open('./etc/{0}.qss'.format('style'), "r").read())
-    main = Widget()
-    main.show()
-    sys.exit(app.exec_())
+print(b1)
